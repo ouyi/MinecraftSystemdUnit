458547c632c024a7c6bd26d852c37c906e8d6a46
Yi Ou <ouyi@gmx.de>
2020-12-15 12:01:44 +0100

    Create minecraft_server.sh

---
+++ minecraft_server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# cat /opt/minecraft/minecraft_server.sh
+
+USAGE="Usage: $0 [server_folder [jar_file]]"
+
+server_folder=${1:-'/opt/minecraft/minecraft_server'}
+jar_file=${2:-'minecraft_server.jar'}
+
+ram_size_mb=$(free -m | grep -F 'Mem:' | awk '{print $2}')
+heap_size_min=$( printf "%0.0f%s" $(echo "$ram_size_mb * 0.7" | bc -l ) 'M' )
+heap_size_max=$( printf "%0.0f%s" $(echo "$ram_size_mb * 0.9" | bc -l ) 'M' )
+
+echo cd "$server_folder" && echo java -server -Xms"$heap_size_min" -Xmx"$heap_size_max" -jar "$jar_file" nogui
