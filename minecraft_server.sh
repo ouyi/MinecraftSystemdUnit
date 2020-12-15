a7d3d8be13777ae3daaeea26bb2e88bd317820b2
Yi Ou <ouyi@gmx.de>
2020-12-15 13:04:22 +0100

    Update minecraft_server.sh

--- minecraft_server.sh
+++ minecraft_server.sh
@@ -12,3 +12,5 @@ heap_size_min=$( printf "%0.0f%s" $(echo "$ram_size_mb * 0.7" | bc -l ) 'M' )
 heap_size_max=$( printf "%0.0f%s" $(echo "$ram_size_mb * 0.9" | bc -l ) 'M' )
 
 cd "$server_folder" && java -server -Xms"$heap_size_min" -Xmx"$heap_size_max" -jar "$jar_file" nogui
+
+# -XX:InitialRAMPercentage=60.0 -XX:MaxRAMPercentage=80.0 -XX:MinRAMPercentage=50.0
